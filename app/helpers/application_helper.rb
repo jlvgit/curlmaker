d5265be7d52d14218cf5b5e03d91e9ad0f0866ab
jvelazquez-sonian <jose.velazquez@sonian.net>
2016-09-02 18:29:51 -0400

    sign in button instead of home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,5 +9,23 @@ module ApplicationHelper
       end
     end
   end
-  
+
+  def all_messages
+
+  # Standard flash messages
+  messages = flash.map{|key,val| {:type=>key, :message=>val} unless val.blank? }.compact
+    #                                                        |-------------------------|
+    # This is where the magic happens. This is how I ignore any blank messages
+
+  # Model validation errors
+  model = instance_variable_get("@#{controller_name.singularize}")
+  unless model.nil?
+    messages += model.errors.full_messages.map do |msg|
+      {:type=>:error, :message=>msg}
+    end
+  end
+
+  return messages
+
+  end
 end
